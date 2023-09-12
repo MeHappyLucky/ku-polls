28c45860af5803db266120c606fd4229a079dd8e
MeHappyLucky <menggy2546@gmail.com>
2023-09-12 15:09:03 +0700

    add force login to vote and some minor changes

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Choice, Question
 
@@ -40,6 +41,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+@login_required
 def vote(request, question_id):
     """ This function handles the voting actions. """
 
